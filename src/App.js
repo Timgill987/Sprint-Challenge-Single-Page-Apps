0fefd505912e305db84b1a28ffcf29438a111b53
Timgill987 <56654688+Timgill987@users.noreply.github.com>
2019-12-20 14:05:36 -0500

    sprint incomplete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { BrowserRouter as Route } from "react-router-dom";
+import CharacterList from "./components/CharacterList";
+import CharacterCard from "./components/CharacterCard.js";
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+
+
+
+      
+      <Route>
+        <Header /> 
+        <CharacterCard/>
+        <CharacterList />
+        
+      </Route>
+
+      
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [Schwifty, setSchwifty] = useState();
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get(`https://rick-api.herokuapp.com/api/`)
+      .then(response => {
+        console.log(response.data.results);
+        setSchwifty(response.data.results);
+      })
+      .catch(error => {
+        console.log("the data was not return", error);
+      });
   }, []);
 
+  //   // TODO: Add API Request here - must run in `useEffect`
+  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <section>
+      {Schwifty.map(item => (
+        
+          <CharacterList
+            key={item}
+            name={item.name}
+            gender={item.gender}
+            species={item.species}
+          />
+        
+      ))}
     </section>
   );
 }
